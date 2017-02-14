eb575b4237cde2f8295260decabea42273d09070
casperjureskog <lcasper@mac.com>
2017-02-14 10:57:21 +0100

    add funktiuon that croects wrong typ height input

--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -6,21 +6,20 @@ describe("Person", function() {
     person = new Person({weight: 90, height: 186});
   });
 
-  it("should have weight of 90", function() {
+  it("should have weight of 90kg", function() {
     expect(person.weight).toEqual(90);
   });
 
-  it("should have height of 186", function() {
-    expect(person.height).toEqual(186);
+  it("should have height of 1.86m", function() {
+    expect(person.height).toEqual(1.86);
   });
 
-  it ("control for height input format" function(){
-    expect(person.height.ctrl).toEqual(1.86)
+  it ("control for height input format", function() {
+    expect(person.height).toEqual(1.86);
   });
-
 
   it("should hav a kalkylator for bmi", function() {
-    expect(bmi).toEqual(90/(1.86*1.86))
+    expect(bmi).toEqual(90/(1.86*1.86));
   });
 
 });
--- src/Person.js
+++ src/Person.js
@@ -2,11 +2,16 @@ function Person(attr) {
     this.weight = attr.weight;
     this.height = attr.height;
 
-    if (this.height < 3){
 
+
+    if (this.height < 3){
       bmi = this.weight/(this.height * this.height);
-      }
+    }
+    else {
+      this.height = this.height/100
       bmi = this.weight/(this.height * this.height);
+    }
+
 
 }
 person = new Person({weight: 90, height: 1.86})
