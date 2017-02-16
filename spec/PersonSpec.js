9da44c766b1830bdd0c5329fdd2484455d2281b2
casperjureskog <lcasper@mac.com>
2017-02-16 15:23:42 +0100

    add extra foot textbox and fixese hidde function

--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -3,30 +3,47 @@ describe("Person", function() {
   var person;
 
   beforeEach(function() {
-    person = new Person({weight: 90, height: 186, metric1: "Metric"});
+    person = new Person({weight: 90, height: 186});
   });
 
   it("should have weight of 90kg", function() {
     expect(person.weight).toEqual(90);
   });
 
-  it("should have height of 186m", function() {
+  it("should have height of 1.86m", function() {
     expect(person.height).toEqual(186);
   });
 
-  it("should calculate BMI value", function() {
+  it("checkbox should have value sett to false at start", function() {
+    person.metric1 = "Metric"
+    $('#calculate').click()
+    expect(person.metric1).toEqual("Metric");
+  });
+
+  it("should have height of 5feet", function() {
+    person.foot1 = 5
+    expect(person.foot1).toEqual(5);
+  });
+
+  it("should calculate BMI value metric", function() {
+    person.metric1 = "Metric"
     person.calculate_bmi();
     expect(person.bmiValue).toEqual(26.01);
   });
 
   it("should have a BMI Message", function() {
+    person.metric1 = "Metric"
     person.calculate_bmi();
     expect(person.bmiMessage).toEqual("Overweight");
   });
 
-  it("checkbox should have value sett to false at start", function() {
-    $('#calculate').click()
-    expect(person.metric1).toEqual("Metric");
+  it("should calculate BMI value Imperial", function() {
+    person.metric1 = "Imperial"
+    person.weight = 189
+    person.height = 2
+    person.foot1 = 6
+    person.calculate_bmi();
+    expect(person.bmiValue).toEqual(24.26);
   });
 
 });
--- src/Person.js
+++ src/Person.js
@@ -2,6 +2,13 @@ function Person(attr) {
     this.weight = attr.weight;
     this.height = attr.height;
     this.metric1 = attr.metric1;
+    this.foot1 = attr.foot1;
+
+    if (this.foot1 > 0){
+      this.height = this.height + (this.foot1 * 12);
+    } else {
+      this.height
+    }
 
 }
 
