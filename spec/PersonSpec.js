3f791dd61b4d61863e9f962d9ee4140956ccaecc
casperjureskog <lcasper@mac.com>
2017-02-17 16:48:31 +0100

    adds CSS styling

--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -3,7 +3,8 @@ describe("Person", function() {
   var person;
 
   beforeEach(function() {
-    person = new Person({weight: 90, height: 186});
+    person = new Person({weight: 90, height: 186, metric1: "Metric"});
+    imp_person = new Person({weight: 198, height: 2, foot1: 6, metric1: "imperial"});
   });
 
   it("should have weight of 90kg", function() {
@@ -14,36 +15,28 @@ describe("Person", function() {
     expect(person.height).toEqual(186);
   });
 
-  it("checkbox should have value sett to false at start", function() {
-    person.metric1 = "Metric"
-    $('#calculate').click()
-    expect(person.metric1).toEqual("Metric");
-  });
-
-  it("should have height of 5feet", function() {
-    person.foot1 = 5
-    expect(person.foot1).toEqual(5);
+  it("should have height of 6 feet", function() {
+    expect(imp_person.foot1).toEqual(6);
   });
 
   it("should calculate BMI value metric", function() {
-    person.metric1 = "Metric"
     person.calculate_bmi();
     expect(person.bmiValue).toEqual(26.01);
   });
 
   it("should have a BMI Message", function() {
-    person.metric1 = "Metric"
     person.calculate_bmi();
     expect(person.bmiMessage).toEqual("Overweight");
   });
 
-  it("should calculate BMI value Imperial", function() {
-    person.metric1 = "Imperial"
-    person.weight = 189
-    person.height = 2
-    person.foot1 = 6
-    person.calculate_bmi();
-    expect(person.bmiValue).toEqual(24.26);
+  it("should calculate BMI value imperial", function() {
+    imp_person.calculate_bmi();
+    expect(imp_person.bmiValue).toEqual(25.42);
+  });
+
+  it("checkbox should have value sett to false at start", function() {
+    $('#calculate').click();
+    expect(person.metric1).toEqual("Metric");
   });
 
 });
