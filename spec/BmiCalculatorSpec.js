c0e4621036fd55f460742c7f5b4a52c3601473f2
casperjureskog <lcasper@mac.com>
2017-02-18 20:45:31 +0100

    adds files to master for io

--- spec/BmiCalculatorSpec.js
+++ spec/BmiCalculatorSpec.js
@@ -3,13 +3,20 @@ describe("BMICalculator", function() {
   var person;
 
   beforeEach(function() {
-    person = new Person({weight: 90, height: 186});
+
     calculator = new BMICalculator();
   });
 
   it("calculates BMI for a person using metric method", function() {
+    person = new Person({weight: 90, height: 186, metric1: "Metric"});
     calculator.metric_bmi(person);
     expect(person.bmiValue).toEqual(26.01);
+  });
+
+  it("calculates BMI for a person using imperial method", function() {
+    person = new Person({weight: 182, height: 70, metric1: "Imperial"});
+    calculator.imperial_bmi(person);
+    expect(person.bmiValue).toEqual(26.11);
   });
 
 });
--- src/Person.js
+++ src/Person.js
@@ -1,9 +1,22 @@
 function Person(attr) {
     this.weight = attr.weight;
     this.height = attr.height;
+    this.metric1 = attr.metric1;
+    this.foot1 = attr.foot1;
+
+    if (this.foot1 > 0){
+      this.height = this.height + (this.foot1 * 12);
+    } else {
+      this.height
+    }
+
 }
 
   Person.prototype.calculate_bmi = function() {
     calculator = new BMICalculator();
-    calculator.metric_bmi(this);
-  };
+    if (this.metric1 == "Metric"){
+      calculator.metric_bmi(this);
+  } else {
+      calculator.imperial_bmi(this);
+    }
+};
